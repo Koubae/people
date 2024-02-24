ac0a419ac12cadc06ff1e7fbf41c46cd969a5db5
Koubae <58447627+Koubae@users.noreply.github.com>
2024-02-24 14:27:56 +0100

    home: addded view as TemplateView

--- app/apps/home/views.py
+++ app/apps/home/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
 
-def index(request):
-    return render(request, "home/index.html")
+class Index(TemplateView):
+    template_name = "home/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["spam"] = [f"egg_{i}" for i in range(10)]
+        return context
