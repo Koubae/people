7bbb510b49707e0ac7e0700c9ddfe458d8cc7fcd
Koubae <58447627+Koubae@users.noreply.github.com>
2024-02-25 18:38:35 +0100

    Added registration htmls and urls

--- app/people/urls.py
+++ app/people/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include("apps.home.urls")),
+    path('accounts/', include("apps.account.urls")),
     path('admin/', admin.site.urls),
 ]
